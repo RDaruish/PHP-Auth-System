a3dbadfe1ffa1e8fb8c2f4f5a7e2bb893511933e
Rafael Daruish Bueno <Rafaeldaruish@gmail.com>
2024-07-27 01:42:29 -0300

    Removendo conteúdo de teste da db

--- auth_db.sql
+++ auth_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27/07/2024 às 06:41
+-- Tempo de geração: 27/07/2024 às 06:42
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -32,17 +32,6 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'montinne', '$2y$10$GpzFzDbcTAZyrHC09sLoCul2voq9u.MLFDLfRMVpse.8SBPuCmyX2'),
-(3, 'teste', '$2y$10$LXLhdsPhnOkSqzP/C0z0wewzUtwAzmWQeYoFfJRIKEiJZldYxHFei'),
-(6, 'montinnea', '$2y$10$V7pZ/A9/jYbqi3aBrT9fXuvgKQajgJXyULIEheG9iRrgtwCz0SiRi'),
-(8, 'montinneaaa', '$2y$10$/1uZOhoMXIwn5b3MOjsLdu0URM90xj5X31GxlllPhgKZwU67ytb72'),
-(10, 'montinneasdasd', '$2y$10$Gq/t8yMexYbgIqH7Tg0bfe.7AGF5eambrGWWICAqYPBI0BNozGvoW');
 
 --
 -- Índices para tabelas despejadas
